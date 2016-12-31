ac8b68b59d4221dad59d18f5302469d10fb3c9dc
Corecii Cyr <Corecii@corecii.com>
2016-12-30 18:06:19 -0600

    Super-optimization
    Make everything local, inline queue functions, remove extra args table

--- ysched.lua
+++ ysched.lua
@@ -1,100 +1,83 @@
 -- optimization
+local getfenv = getfenv
+local string_match = string.match
+local assert = assert
+local loadfile = loadfile
+local error = error
+
+local debug = debug
+local debug_getinfo = debug.getinfo
+
+local table_insert = table.insert
+local table_remove = table.remove
+
+local coroutine_resume = coroutine.resume
+local coroutine_yield = coroutine.yield
+
+local no_args = {}
 
 local os_clock = os.clock
-local noargs = {}
 
 -- import hack
-
 local oldrequire = require
 require = function(argname)
-    if not getfenv().wait or not argname:match("ysched") then
+    if not getfenv().wait or not string_match(argnmae, "ysched") then
         return oldrequire(argname)
     end
 end
 
 -- finds function to schedule
-
 local target
 if arg[1] then
     target = assert(loadfile(arg[1]))
 elseif debug then
-    target = debug.getinfo(3, "f").func -- cheeky hack to support require
+    target = debug_getinfo(3, "f").func -- cheeky hack to support require
 else
     error "[SCHEDULER] Error: No arguments or being required without debug library."
 end
 
 -- queue code 
-
-local queue = {}
-
-function queue:push(task, back)
-    if back then
-        self[#self + 1] = task
-        return
-    end
-    table.insert(self, 1, task)
-end
-
-function queue:pop()
-    return table.remove(self, 1)
-end
+local queue = {
+    {
+        coroutine = coroutine_create(_function)
+    }
+}
 
 -- scheduler apis 
-
 function spawn(_function)
-    queue:push {
-        ["coroutine"] = coroutine.create(_function),
-        ["condition"] = false,
-        ["args"] = noargs,
-    }
+    table_insert(queue, 1, {
+        coroutine = coroutine_create(_function)
+    })
 end
 
-function yield(condition, _function, ...) -- ... = args for _function 
-    local args       = {...}
-    local truth      = condition
-    local running    = coroutine.running()
-    local _coroutine = _function and coroutine.create(function(...) _function(...) coroutine.resume(running) end) or coroutine.running()
-
-    queue:push {
-        ["coroutine"] = _coroutine,
-        ["condition"] = truth,
-        ["args"]      = args,
-    }
-
-    return coroutine.yield()
+function yield(condition, _function, ...)  -- ... = args for _function
+    local running = coroutine_running()
+    table_insert(queue, 1, {
+        coroutine = _function and coroutine.create(function(...) _function(...) coroutine_resume(running) end) or running,
+        condition = condition,
+        ...
+    })
+    return coroutine_yield()
 end
 
 function wait(time)
-    local _coroutine = coroutine.running()
-
-    queue:push {
-        ["coroutine"] = _coroutine,
-        ["args"] = noargs,
-        ["resumeat"] = os_clock() + time,
-    }
-
-    return coroutine.yield()
+    table_insert(queue, 1, {
+        coroutine = coroutine_running(),
+        resumeat = os_clock() + time
+    })
+    return coroutine_yield()
 end
 
---starts scheduling code
-
-local function startscheduler()
-    local now, step
-    repeat
-        now = os_clock()
-        step = queue:pop()
-        assert(step, "No step means that something went horribly, horribly wrong")
-        if (not step.condition and not step.resumeat) or (step.resumeat and now >= step.resumeat) or (step.condition and step.condition()) then
-            coroutine.resume(step.coroutine, unpack(step.args))
-        else
-            queue:push(step, true)
-        end
-    until #queue    == 0
-    debug.traceback = nil -- remove stacktrace from error in vanilla lua 
-    error "[SCHEDULER] Execution complete." -- prevent script from running again when required. (side effect of require hack)
-end
-
--- init
-
-spawn(target) -- put specified function on scheduler 
-startscheduler() -- starts scheduling
+-- start scheduler
+local now, step
+repeat
+    now = os_clock()
+    step = table_remove(queue, 1)
+    if (not step.condition and not step.resumeAt) or (step.resumeAt and now >= step.resumeAt) or (step.condition and step.condition()) then
+        coroutine_resume(step.coroutine, unpack(step))
+    else
+        queue[#queue + 1] = step
+    end
+until #queue    == 0
+debug.traceback = nil -- remove stacktrace from error in vanilla lua 
+error "[SCHEDULER] Execution complete." -- prevent script from running again when required. (side effect of require hack)
